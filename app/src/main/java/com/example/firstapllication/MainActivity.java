2b6bcf0a13a53459a8c362530c4e7e8d31b762dd
cobbaheugene <cobbaheugene@gmail.com>
2022-08-06 22:42:56 +0000

    update

--- app/src/main/java/com/example/firstapllication/MainActivity.java
+++ app/src/main/java/com/example/firstapllication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.firstapllication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtHello = findViewById(R.id.txtMessage);
+        txtHello.setText("Hello");
     }
 }
